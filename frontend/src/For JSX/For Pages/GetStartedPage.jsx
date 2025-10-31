5b3e4e3bb4be4748771df5de2492827242b9dcf2
Kehinde Imani <imamikenny27@gmail.com>
2025-10-31 19:06:50 +0100

    Added Landing page and GetStarted

---
+++ frontend/src/For JSX/For Pages/GetStartedPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function GetStartedPage() {
+    return(
+        <div className="get-started">
+            <div className="image-section">
+                <img src="https://c8.alamy.com/comp/2AF2X77/happy-lady-listening-to-music-through-wireless-headphones-2AF2X77.jpg" alt="Mental Wellness" />
+            </div>
+            <div className="button-section">
+                <h1>Your Mental Health Simplified</h1>
+                <p>All your mental problems solution</p>
+                <div className="buttons">
+                    <Link to="/register"><button className="primary-btn">Sign Up</button></Link>
+                    <button className="secondary-btn">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
